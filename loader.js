c3a3fa0a73138e093525732c80a6dbbe9526f5ad
martinhlawiczka1234-crypto <martin.hlawiczka1234@gmail.com>
2026-03-06 09:28:01 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,21 +1,21 @@
 (function(){
 
-function addScript(url){
-  var s = document.createElement("script");
-  s.src = url;
-  s.defer = true;
-  document.head.appendChild(s);
-}
+  function addScript(url){
+    var s = document.createElement("script");
+    s.src = url;
+    s.defer = true;
+    document.head.appendChild(s);
+  }
 
-function addCSS(url){
-  var l = document.createElement("link");
-  l.rel = "stylesheet";
-  l.href = url;
-  document.head.appendChild(l);
-}
+  function addCSS(url){
+    var l = document.createElement("link");
+    l.rel = "stylesheet";
+    l.href = url;
+    document.head.appendChild(l);
+  }
 
-/* sem budeš přidávat další kód */
+  addCSS("https://cdn.jsdelivr.net/gh/martinhlawiczka1234-crypto/shoptet-assets@main/style.css");
 
-console.log("Shoptet loader running");
+  console.log("Shoptet loader running");
 
 })();
